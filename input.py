07338517165583b966f19b9c466515c3ac25f15e
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-01-03 23:31:13 +0530

    Update input.py

--- input.py
+++ input.py
@@ -1,10 +1,11 @@
 # types of input
 
-#a=input("enter num")
- # print(a)
+a=input("enter num")
+print(a)
 
- # name=input("enter name: ")
- # print(name)
+name=input("enter name: ")
+print(name)
 
 f=input("enter decimal num")
 print(f)
+
