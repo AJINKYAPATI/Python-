633a7cf803d01e84b2a5a8ec0f2461a0c7537dcf
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-01-08 15:06:35 +0530

    Create days.py

---
+++ days.py
@@ -0,0 +1,19 @@
+
+print('choose day from 1 to 7:')
+day=int(input("enter day:"))
+if day==1:
+    print("monday")
+elif day==2:
+    print("tuesday")
+elif day==3:
+    print("wednesday")
+elif day==4:
+    print("thursday")
+elif day==5:
+    print("friday")
+elif day==6:
+    print("saturday")
+elif day==7:
+    print("sunday")
+else:
+    print("invalid")
