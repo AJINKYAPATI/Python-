4309e1f01250c8bfd1baf156ce7c3704c53cd469
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-01-08 15:05:16 +0530

    Create num.py

---
+++ num.py
@@ -0,0 +1,12 @@
+num=int(input("enter a number:"))
+if num>=35 and num<=50:
+    print("poor")
+elif num>=51 and num<=70:
+    print("Average")
+elif num>=71 and num<=90:
+    print("Good ")
+elif num>=91 and num<=100:
+    print("Excellent")
+else:
+    print("invalid no")
+    
