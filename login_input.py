2183f76dd37952e061f9bdded99f18e9b4bd3c17
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-01-08 15:05:58 +0530

    Create login_input.py

---
+++ login_input.py
@@ -0,0 +1,12 @@
+username=input("enter username: ")
+password=input("enter password: ")
+if username == "admin" and password == "1234":
+    print("login successful")
+elif username == "admin" and password != "1234":
+        print("invalid password")
+elif username != "admin" and password == "1234":
+        print("invalid username")   
+
+else:
+    print("login fail")
+
