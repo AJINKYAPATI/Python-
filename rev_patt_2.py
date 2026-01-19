6babbf739867e4ff00049799d9c0b0dde5a19edc
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-01-19 22:29:07 +0530

    Create rev_patt_2.py

---
+++ rev_patt_2.py
@@ -0,0 +1,12 @@
+n=4
+i=n
+while i>=1:
+    j=1
+    while j<=i:
+        if i%2==0:
+            print("1",end="")
+        else:
+            print("0",end="")
+        j+=1
+    print("")
+    i-=1
