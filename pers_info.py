5057507032d4574be2a53abc7e06d1b6f0cc5512
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-03-06 23:19:31 +0530

    Fix print statement for mobile number

--- pers_info.py
+++ pers_info.py
@@ -1,8 +1,13 @@
 fullname=input("Enter your full name\n ")
+
 favsub=input("Enter your favorite subject\n")
+
 age=input("Enter your age\n ")   
+
 email=input("Enter your email address\n ")   
+
 mobile=input("Enter your mobile number\n")  
+
 print("Full Name: ",fullname)
 print("Favorite Subject: ",favsub)
 print("Age: ",age)
