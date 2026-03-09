efadffbaa786404b79b37d6296792bbf8598c5ea
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-03-10 00:01:27 +0530

    Fix indentation for invalid number message

--- num.py
+++ num.py
@@ -1,4 +1,5 @@
 num=int(input("enter a number:"))
+
 if num>=35 and num<=50:
     print("poor")
 elif num>=51 and num<=70:
@@ -7,6 +8,6 @@ elif num>=71 and num<=90:
     print("Good ")
 elif num>=91 and num<=100:
     print("Excellent")
+
 else:
     print("invalid no")
-    
