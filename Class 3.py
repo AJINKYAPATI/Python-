eae31dc189427c0577312d96d827d724c9b5e4bd
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-03-11 14:19:37 +0530

    Create Class 3.py

---
+++ Class 3.py
@@ -0,0 +1,10 @@
+class student:
+    def __init__(self,name,age,city):
+        self.name=name
+        self.age=age
+        self.city=city
+
+s1=student("rahul",23,"pune")
+s2=student("ajinkya",24,"mumbai")
+print(s1.name,s1.age,s1.city)
+print(s2.name,s2.age,s2.city)
