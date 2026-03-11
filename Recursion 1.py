fd77dd16bbad4d587dea82139283f9c708f391d4
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-03-11 14:15:52 +0530

    Create Recursion 1.py

---
+++ Recursion 1.py
@@ -0,0 +1,15 @@
+#loop
+def fact(n):
+    power=1
+    for i in range(1,n+1):
+        power*=i
+    return power
+print(fact(5))
+
+#Recursion
+def fact1(n):
+    if n==1 or n==0:
+        return 1
+    else:
+        return n*fact1(n-1)
+print(fact1(5))
