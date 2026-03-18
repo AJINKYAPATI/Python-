ab2d0dcb57d8e25602acee4c9040f7ccd34c2a45
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-03-18 23:46:12 +0530

    Update armstrong_num.py

--- armstrong_num.py
+++ armstrong_num.py
@@ -1,10 +1,12 @@
 num=int(input("enter a number :"))
 temp=num
 sum=0
+
 while num>0:
     digit=num%10
     sum=sum+digit**3
     num=num//10
+
 if sum==temp:
     print("it is an armstrong number")
 else:
