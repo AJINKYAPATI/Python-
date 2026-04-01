d0da5478d3c87fb6c36b3584ac8caacde6f0e8df
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-04-01 21:47:16 +0530

    Implement integer to float conversion program
    Added a program to convert user input from integer to float.

---
+++ Pratice_set_1.5.py
@@ -0,0 +1,8 @@
+# Write a program to take an user input as integer
+# then convert it to float.
+a=int(input("enter your name here: "))
+print("a")
+print(type(a))
+a=float(a)
+print("after conversition: ", a)
+print(type(a))
