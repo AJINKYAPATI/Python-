1964425fdd993f82bb90979cd74cb1b4c3ce5c4e
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-04-01 21:46:36 +0530

    Add student details input and output program

---
+++ Pratice_set_1.4.py
@@ -0,0 +1,12 @@
+# Write a program to take details from a student for
+# ID-card and then print it in different lines.
+print ("student data")
+name=input("enter your name: ")
+grade=input("enter your grade: ")
+age=int(input("enter your age: "))
+email=input("enter your email: ")
+print("student adentity")
+print("name:", name)
+print("grade:", grade)
+print("age:", age)
+print("email:", email)
