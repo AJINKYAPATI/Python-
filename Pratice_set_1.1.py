ad34663565198ab0d4a09856a8d083e81b9f4e0b
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-04-01 21:44:14 +0530

    Create Pratice_set_1.1.py

---
+++ Pratice_set_1.1.py
@@ -0,0 +1,10 @@
+#Write a program to display a person's name, age and
+# address in three different lines.
+
+
+name = "Ajinkya"
+age = 22
+address = "At post bodwad" 
+print(name)
+print(age)
+print(address)
