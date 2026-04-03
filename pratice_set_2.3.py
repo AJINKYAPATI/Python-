12fed233b12a7bafb8ab6fcfce636df90a2fcec0
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-04-03 20:33:05 +0530

    Create pratice_set_2.3.py

---
+++ pratice_set_2.3.py
@@ -0,0 +1,6 @@
+# #Write a program to check whether a number is odd or even.
+num=int(input("enter your number here: "))
+if num % 2 == 0:
+    print("your number is odd")
+else:
+    print("your number is even number")
