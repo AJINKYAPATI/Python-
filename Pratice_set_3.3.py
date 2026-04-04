025396ab86eb87f9fe3f424055adf0b83651d722
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-04-05 00:39:14 +0530

    Create Pratice_set_3.3.py

---
+++ Pratice_set_3.3.py
@@ -0,0 +1,9 @@
+# Write a program to find sum of first 10 odd numbers
+# using while loop.
+sum=0
+n=0
+while n <=20:
+    if n % 2 != 0:
+        sum += n
+    n += 1
+print("The sum of first 10 odd numbers is ", sum)
