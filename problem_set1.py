f032c5109864dda2369fb314382f24225d8b52e6
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-04-09 11:58:56 +0530

    Create problem_set1.py

---
+++ problem_set1.py
@@ -0,0 +1,28 @@
+a = "Why fit in, When you are Born to Stand Out!"
+
+# Write a program to find the length of the following string.
+print(len(a))
+
+
+# Write a program to check how many time alphabet o is occurring.
+print("The number of times o is occuringis", a.count("o"))
+
+
+
+
+# Write a program to convert the whole string into lower
+# and upper cases.
+x = a.lower() 
+print(x)
+y = a.upper()
+print(y)
+
+
+# Write a program to convert the following string into a title.
+z = a.title()
+print(z)
+
+
+
+#write aprogram to find index number of "fit in".
+print (a.find("fit in"))
