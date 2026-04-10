07ecaaa678e4223bc5d837c0406105dad350cf00
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-04-10 10:35:00 +0530

    Update sets_problem.py

--- sets_problem.py
+++ sets_problem.py
@@ -17,3 +17,18 @@ a = {1,5,6,8,2}
 b = {1,9,6,2,5}
 
 print (b.difference(a))
+
+# Write a Python program to remove an item from
+# a set if it is present in the set.
+a = {1,5,6,8,2}
+a.discard(5)
+print(a)
+ 
+
+# Write a Python program to check if a set
+# is a subset of another set.
+a = {1,2,3,4,5,6}
+b = {2,3,4}
+print(b.issubset(a))
+
+
