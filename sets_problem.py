d40e59acae77b9704de2dc64958cb0b4b6974dbd
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-04-10 10:26:34 +0530

    Update sets_problem.py

--- sets_problem.py
+++ sets_problem.py
@@ -4,3 +4,10 @@ maximum = max(a)
 minimum = min(a)
 print ("The minimum value in the set is :", minimum)
 print ("The maximum value in the set is :", maximum)
+
+# Write a program to find common elements in
+# three lists using sets.
+a = [1,5,6,8,2]
+b = [4,5,6,7]
+c = [1,9,6,2,5]
+print("The common elements in the three lists are: ", set(a) & set(b) & set(c))
