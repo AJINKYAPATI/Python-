ec5e42dcd6d803894b1d2619e16d7e19497f4914
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-04-10 10:21:50 +0530

    Create sets_problem.py

---
+++ sets_problem.py
@@ -0,0 +1,6 @@
+# Write a program to find max and min in a set.I
+a= {12, 34, 56, 78, 90, 1, 57}
+maximum = max(a)
+minimum = min(a)
+print ("The minimum value in the set is :", minimum)
+print ("The maximum value in the set is :", maximum)
