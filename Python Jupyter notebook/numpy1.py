f770dbe64774c2911a531ee21fd423d977e99b94
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-04-11 18:36:32 +0530

    Create numpy1.py

---
+++ Python Jupyter notebook/numpy1.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+a = [[30,40,40],[40,30.8,10]
+arr = np.array(a)
+print(arr)
+print(arr.shape) #row, columns
+print(len(arr))  #number of nested values
+print(np.size(arr))  #number of elements
+print(type(arr))  #datatype of variable
+print(arr.dtype)  #data type array
+print(arr.astype(int))  #conversion of datatype
