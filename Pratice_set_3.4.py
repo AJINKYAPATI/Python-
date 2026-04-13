4e94e79bf405d8692f10ded10f28cf8fe2fc28f2
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-04-13 23:50:29 +0530

    Update Pratice_set_3.4.py

--- Pratice_set_3.4.py
+++ Pratice_set_3.4.py
@@ -1,6 +1,5 @@
 # Write a program to check if a number is divisible by
 # 8 and 12, up to 100 numbers
-
 for i in range(1,101):
     if i % 8 == 0 and i % 12 == 0:
         print(i, "is divisible by both 8 and 12.")
