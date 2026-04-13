5a1408cd36422364730bcdfc4c1e4687aa1015ac
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-04-13 23:49:34 +0530

    Update Pratice_set_3.1.py

--- Pratice_set_3.1.py
+++ Pratice_set_3.1.py
@@ -1,8 +1,7 @@
 # Write a program to find a sum of all the even numbers up to 50.
+
 sum=0
 for i in range(1,51):
     if i % 2 == 0:
         sum += i
     print("The sum of all the even numbers up to 50 is ", sum)
-
-
