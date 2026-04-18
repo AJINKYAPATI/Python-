de06a6026d44e80f584c678427859ad2ce23efe1
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-04-19 00:34:41 +0530

    Update list_pratice_set2.py

--- list_pratice_set2.py
+++ list_pratice_set2.py
@@ -1,15 +1,13 @@
 B = [13 , 7 , 12 , 10]
+
 # Write a program to multiply all the numbers in the list.
 mul = 1
 for i in (B):
     mul*=i
     print(mul)
 
-
 # Write a program to get the largest number from the list.
 print(max(B))
-
-
 
 # Write a program to get the smallest number from the list.
 print(min(B))
