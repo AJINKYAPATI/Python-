3f5a682687d2ea81ccf6d9e6e6611d53e54128b0
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-04-23 23:17:44 +0530

    Update list_pratice_set1.py

--- list_pratice_set1.py
+++ list_pratice_set1.py
@@ -4,10 +4,10 @@ A= ["Ross" , "Rachel" , "Monica" , "Joe"]
 A[0],A[3]=A[3],A[0]
 print (A)
 
+
 # Write a program to add a new value at second position.
 A.insert(1, "phoebe")
 print(A)
-
 
 # Write a program to delete a value from 3rd position.
 A.pop(2)
