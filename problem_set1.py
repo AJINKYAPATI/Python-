503b376f42449a35bd090d5f4d450ed8c5da45ca
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-04-25 22:53:17 +0530

    Update problem_set1.py

--- problem_set1.py
+++ problem_set1.py
@@ -1,13 +1,12 @@
 a = "Why fit in, When you are Born to Stand Out!"
 
+
 # Write a program to find the length of the following string.
 print(len(a))
 
 
 # Write a program to check how many time alphabet o is occurring.
 print("The number of times o is occuringis", a.count("o"))
-
-
 
 
 # Write a program to convert the whole string into lower
