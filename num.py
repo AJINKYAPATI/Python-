143a09ea2153b3adc405f80fa90a34e6ba3155a2
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-05-01 23:49:31 +0530

    Update num.py

--- num.py
+++ num.py
@@ -2,10 +2,13 @@ num=int(input("enter a number:"))
 
 if num>=35 and num<=50:
     print("poor")
+
 elif num>=51 and num<=70:
     print("Average")
+
 elif num>=71 and num<=90:
     print("Good ")
+
 elif num>=91 and num<=100:
     print("Excellent")
 
