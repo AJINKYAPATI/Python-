eecd3b09d48789d7fd084cc586d4b21177cdf694
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-05-08 22:54:45 +0530

    Update Pratice_set_4.2.py

--- Pratice_set_4.2.py
+++ Pratice_set_4.2.py
@@ -1,7 +1,9 @@
 # Write a program to check if a number is prime or not.
 
+
 n = int(input("enter a number here: "))
 if n > 1:
+    
     for i in range(2, n):
         if (n % i) == 0:
             print(n, "is not a prime number")
