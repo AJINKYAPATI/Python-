33c2170bb0b8aeb06a99b23707d12f32d19f0d31
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-05-14 18:10:43 +0530

    Update Pratice_set_3.5.py

--- Pratice_set_3.5.py
+++ Pratice_set_3.5.py
@@ -1,4 +1,5 @@
 # # write a program to create a building system at supermarket.
+
 while True:
     name = input("Enter customer name: ")
     total = 0
@@ -13,7 +14,8 @@ while True:
         repeat = input("Do you want to add more items? (yes/no): ").lower()
         if repeat == "no":
             break
- # Bill printing (outside inner loop)
+
+# Bill printing (outside inner loop)
     print("\n" + "-" * 40)
     print("Name:", name)
     print("Amount to be paid: ₹", total)
